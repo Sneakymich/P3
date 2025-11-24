998a1e62b0fa445b630e4672970b1b78e614b993
Inigo Michelena Mayorgas <inigo.michelena@estudiantat.upc.edu>
2025-11-24 15:08:44 +0100

    full de cambios manin

--- src/get_pitch/pitch_analyzer.cpp
+++ src/get_pitch/pitch_analyzer.cpp
@@ -13,7 +13,7 @@ namespace upc {
     for (unsigned int l = 0; l < r.size(); ++l) {
   		/// \TODO Compute the autocorrelation r[l]
       r[l] = 0.0F;
-      for (unsigned int n = 0; n < r.size() - l; ++n) {
+      for (unsigned int n = 0; n < x.size() - l; ++n) {
         r[l] += x[n] * x[n + l];
       }
       /**
@@ -60,7 +60,11 @@ namespace upc {
     /// \TODO Implement a rule to decide whether the sound is voiced or not.
     /// * You can use the standard features (pot, r1norm, rmaxnorm),
     ///   or compute and use other ones.
-    return false;
+
+    if(rmaxnorm > this->umaxnorm)
+      return false;
+    else
+    return true;
   }
 
   float PitchAnalyzer::compute_pitch(vector<float> & x) const {
@@ -88,11 +92,10 @@ namespace upc {
 
     vector<float>::const_iterator iRStart = r.begin() + npitch_min;
     vector<float>::const_iterator iREnd = r.end();
-      
-    vector<float>::const_iterator iRMax = max_element(iRStart, iREnd);
-    
-    unsigned int lag = iRMax - r.begin();
 
+    iRMax = max_element(iRStart, iREnd); // Asignar, no sredeclarar
+
+    unsigned int lag = iRMax - r.begin();
     float pot = 10 * log10(r[0]);
 
     //You can print these (and other) features, look at them using wavesurfer
